ccf31ab8eb6f0da80bd1b47f1b6161fff12b88bc
Mikhail Chugunkov <poslegm@gmail.com>
2023-01-31 23:47:43 +0000

    htop -> btop

--- tmux/default-session.sh
+++ tmux/default-session.sh
@@ -4,5 +4,5 @@ tmux split-window -v -c "#{pane_current_path}"
 tmux select-pane -t 0
 tmux split-window -v -c "#{pane_current_path}"
 tmux select-pane -t 3
-tmux send-keys "htop" C-m
+tmux send-keys "btop" C-m
 tmux select-pane -t 0
