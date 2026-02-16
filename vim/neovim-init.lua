7eee9f11eab8c0125c5bee5a7c883777fa082a88
Mikhail Chugunkov <poslegm@gmail.com>
2026-02-17 01:06:32 +0300

    reenable cmp

--- vim/neovim-init.lua
+++ vim/neovim-init.lua
@@ -1,3 +1,51 @@
 vim.api.nvim_command('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.api.nvim_command('let &packpath = &runtimepath')
 vim.api.nvim_command('source ~/.vimrc')
+
+local api = vim.api
+local cmd = vim.cmd
+local map = vim.keymap.set
+
+-- global
+vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'path' },
+  })
+})
